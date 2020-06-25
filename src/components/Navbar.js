d9ab459780bef0b2b76dd36a58c9e7c7ce0b0b63
Sam McHaney <luomopuma@gmail.com>
2020-06-24 19:14:35 -0700

    Set up temporary page.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import worx from '../img/logo-color-transparent.png'
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <img src={worx} alt="Worx-logo" style={{ width: '88px', maxHeight: 'initial' }} />
             </Link>
             {/* Hamburger menu */}
             <div
